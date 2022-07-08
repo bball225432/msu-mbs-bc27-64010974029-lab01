8afd75b6c8e44cc95c8b0ac06e488cbe5d1a01f4
oatsadawut <108518864+bball225432@users.noreply.github.com>
2022-07-08 15:23:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>My id 64010974029</p>'; ?> 
+   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+     <?php echo '<p>Update-1st</p>'; ?> 
+    
+ </body>
+</html>
