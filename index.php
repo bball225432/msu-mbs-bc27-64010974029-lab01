2bc6505efaeb0c7dd4aa0bf0cfa89f6b19cfe335
oatsadawut <108518864+bball225432@users.noreply.github.com>
2022-07-08 15:25:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
    <?php echo '<p>My id 64010974029</p>'; ?> 
-   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+   <?php echo '<p>My name is oatsadawut</p>'; ?> 
      <?php echo '<p>Update-1st</p>'; ?> 
+       <?php echo '<p>Update-2st</p>'; ?> 
     
  </body>
 </html>
